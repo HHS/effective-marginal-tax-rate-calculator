cc82b6562a2316c04b8133d26928b40926335a67
NinaChien-ASPE <137441368+NinaChien-ASPE@users.noreply.github.com>
2023-08-03 12:40:21 -0400

    Add files via upload

---
+++ helpwithbenefitcliffs.php
@@ -0,0 +1,51 @@
+<head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather|Source Sans Pro">
+<style>
+body {
+  font-family: "Merriweather", sans-serif;
+    font-size: 16px;
+    }
+</style>
+</head>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+
+<meta charset="utf-8">
+<meta name="description" content="" />
+
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700|Oswald:400,700" rel="stylesheet"> 
+<link rel="stylesheet" href="/css/text.css?v=1"   type="text/css" media="all" />
+<link rel="stylesheet" href="/css/print.css"  type="text/css" media="print" />
+<link rel="stylesheet" href="/css/menu.css?v=1"   type="text/css" media="screen" title="modern" />
+<link rel="stylesheet" href="/css/layout.css?v=1" type="text/css" media="screen" title="modern" />
+<link rel="alternate stylesheet" href="/css/basic.css" type="text/css" media="screen" title="basic" />
+
+<!--[if IE 9]>
+<link rel="stylesheet" href="/css/ie9.css"   type="text/css" media="all" />
+<![endif]-->
+<link rel="stylesheet" href="frs.css" type="text/css" media="screen" />
+
+<script type="text/javascript">
+
+</script>
+</head><body><header></header>
+
+<div id="content" class="content frs grid_8 alpha">
+<h1>Benefit Cliffs Calculator - Tips</h1>
+
+Tips for avoiding benefit cliffs, dealing with benefit loss, increasing your income, or lowering your expenses: <br/>
+<br/>
+<ul>
+<li>Ask your case manager about redetermination periods. Benefit changes do not always take effect right away. If the next time you will report your income is a long time from now, you will have time to prepare for the changes.</li>
+<li>Talk to your case manager about any benefits that are lower in the "New Situation" column. Some benefits phase out slowly over time. Your case manager can give you more information.</li>
+<li>If you receive housing assistance, ask your landlord or case manager about "flat rents."</li>
+<li>If the calculator shows higher child care costs in the "New Situation" column, ask your case manager about child care subsidies, Head Start, Early Head Start, or affordable pre-K or afterschool programs.</li>
+<li>If you receive or pay child support and higher child care costs are part of the reasson for the change in your benefits, you may be able to adjust your child support order to help cover those costs.</li> 
+<li>Ask your employer(s) about flexible spending accounts (FSAs), like child care FSAs or medical FSAs. These can help lower your taxes. Medical FSAs  may help you or your children stay on Medicaid. If you have health insurance from the marketplace (HealthCare.gov), FSAs may help lower your health care payments.</li>
+<li>Ask your employer(s) about “tax-advantaged retirement accounts.” These help you save money for when you’re older. They can also lower your taxes. They may help lower your health insurance costs.</li> 
+<li>If you or someone in your houshold is earning income and have a disability, you may be able to prevent the loss of some benefits by depositing money into an ABLE account, a special type of bank account.</li>
+<li>Some employers offer on-site child care or employee shuttles. These can help lower your child care or transportation costs.</li>
+</ul>
+<br/>
