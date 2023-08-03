90875d4bb1cbe3846e5aea8b0ee6679cbbb6303c
NinaChien-ASPE <137441368+NinaChien-ASPE@users.noreply.github.com>
2023-08-03 12:38:06 -0400

    Add files via upload

---
+++ intro_nh.php
@@ -0,0 +1,65 @@
+<head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather|Source Sans Pro">
+<style>
+body {
+  font-family: "Merriweather", sans-serif;
+    font-size: 16px;
+    }
+</style>
+</head>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+
+<title>MTRC | Family Resource Simulator</title>
+<meta charset="utf-8">
+<meta name="description" content="" />
+
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700|Oswald:400,700" rel="stylesheet"> 
+<link rel="stylesheet" href="/css/text.css?v=1"   type="text/css" media="all" />
+<link rel="stylesheet" href="/css/print.css"  type="text/css" media="print" />
+<link rel="stylesheet" href="/css/menu.css?v=1"   type="text/css" media="screen" title="modern" />
+<link rel="stylesheet" href="/css/layout.css?v=1" type="text/css" media="screen" title="modern" />
+<link rel="alternate stylesheet" href="/css/basic.css" type="text/css" media="screen" title="basic" />
+
+<!--[if IE 9]>
+<link rel="stylesheet" href="/css/ie9.css"   type="text/css" media="all" />
+<![endif]-->
+<link rel="stylesheet" href="frs.css" type="text/css" media="screen" />
+
+<script type="text/javascript">
+
+</script>
+</head><body><header></header>
+
+<div id="content" class="content frs grid_8 alpha">
+<h1>Benefit Cliffs Calculator</h1>
+
+Use this tool to see how your benefits may change when your earnings change.<br/><br>
+You will:<br><br>
+1. Enter basic information about your household.<br/>
+2. Enter how your earnings will change. You can change how many hours you work, or your hourly wage, or both.<br/>
+3. Answer more questions about your household.<br/>
+4. See how your resources, benefits, and expenses will change.<br/>
+5. Find out what to do next.<br><br>
+Note: This tool does not work for all households. It will not be accurate if your household includes any of these:<br/>
+- People currently without housing<br/>
+- People age 65 or older<br/> 
+- People who receive Medicare or Social Security benefits<br/>
+- Children who receive federal or state disability benefits<br/>
+- Young adult students on SSI (Supplemental Security Income)<br/>
+- More than 5 children<br/>
+- More than 4 adults<br/>
+- People convicted of a felony that may result in ineligibility for benefits<br/>
+- Foster children<br/>
+- Children only, with no adult household members<br/>
+- Current or former military service members<br/>
+- Someone whose spouse died in the last two years<br/>
+- People who qualify for programs supporting American Indian populations<br>
+- Two or more married couples living together<br>
+- A married person whose spouse does not live in the household<br><br>
+If your household has any of these, do not use this tool. Call the XXX office at xxx-xxx-xxxx to find out how your benefits may change or ask your case manager.<br>
+
+<br/>
+<p><a href="nh.php"  target="_blank">Get Started</a></p>
